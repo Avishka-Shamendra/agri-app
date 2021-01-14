1dfa00be04701f2163f0fdf1b2945eaaf85aeff1
unknown <kasun7153@gmail.com>
2021-01-14 17:06:28 +0530

    my

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,10 @@ const ifLoggedIn = require('../middleware/ifLoggedIn');
 const ifNotLoggedIn = require('../middleware/ifNotLoggedIn');
 
 router.get('/', ifNotLoggedIn, RootController.indexPage);
+
 router.get('/login',ifNotLoggedIn, RootController.loginPage);
 router.post('/login', ifNotLoggedIn, RootController.login);
+
 router.get('/logout', ifLoggedIn, RootController.logout);
 
 router.use('/admin', require('./admin'));

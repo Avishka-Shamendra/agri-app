cdf9471b53b2be2ef68878afb7c8b250ac1f07c3
Avishka-Shamendra <avishka199849@gmail.com>
2021-01-14 17:41:40 +0530

    added max to price

--- controllers/validators/postInfo.js
+++ controllers/validators/postInfo.js
@@ -4,7 +4,7 @@ const addpostInfo = Joi.object().options({ abortEarly: false }).keys({
     title:Joi.string().max(50).required().label("Post Title"),
     product_name:Joi.string().max(50).required().label("Product Name"),
     quantity:Joi.number().integer().min(0).less(1000000).required().label("Quantity"),
-    expected_price:Joi.number().integer().min(0).required().label("Expected Price"),
+    expected_price:Joi.number().integer().min(0).less(1000000).required().label("Expected Price"),
     description:Joi.string().max(800).required().label("Description"),
     phone_num:Joi.string().trim().required()
     .length(10, 'utf8')

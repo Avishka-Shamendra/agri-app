5d7e556d6765852b5e959888a88cdb65809f8189
unknown <kasun7153@gmail.com>
2021-01-24 19:15:40 +0530

    make it bette

--- config/db.js
+++ config/db.js
@@ -1,5 +1,8 @@
 const postgres = require('postgres');
 
-const sql = postgres(process.env.DATABASE_URL);
+const sql = postgres({connectionString:process.env.DATABASE_URL,
+    ssl:{
+        rejectUnauthorized: false
+    }});
 
 module.exports = sql;
